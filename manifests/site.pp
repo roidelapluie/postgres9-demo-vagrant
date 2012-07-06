e98ca54d7b8b916734fb535b4bfbee4315eebacd
Julien Pivotto <roidelapluie@inuits.eu>
2012-07-06 15:08:55 +0200

    update the demo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,21 @@
 node postgres-server {
   include postgres
-}
+  include site
 
+  class {
+    'cluster':
+      cluster_bind_interface => 'eth1',
+      cluster_mcastaddr      => '226.94.1.1',
+  }
+
+  include site::sshkeys
+
+  Class['site'] -> Class['cluster']
+  Postgres::Cluster['cluster'] -> Class['cluster']
+
+}
 node dupont inherits postgres-server {
+  postgres::initdb { 'initdb': }
   postgres::cluster {
     'cluster':
       other_node_ip        => '192.168.142.32',
@@ -12,6 +25,7 @@ node dupont inherits postgres-server {
       work_mem             => '3MB',
       effective_cache_size => '352MB',
   }
+  Class['postgres'] -> Postgres::Initdb['initdb'] -> Postgres::Cluster['cluster']
 }
 
 node dupond inherits postgres-server {
@@ -24,4 +38,8 @@ node dupond inherits postgres-server {
       work_mem             => '3MB',
       effective_cache_size => '352MB',
   }
+  Class['postgres'] -> Postgres::Cluster['cluster']
+  include postgres::firstsync
+
+  Class['cluster'] -> Class['postgres::firstsync']
 }
